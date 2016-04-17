012dcf22b43e312e45cafa7b6e4bd82a7ac416b6
muramasa8191 <tamuram@onid.orst.edu>
2016-04-16 20:41:36 -0700

    Update enumeration v4.py
    Added timing, file input and output, and returning the sub-array

--- enumeration v4.py
+++ enumeration v4.py
@@ -48,29 +48,91 @@
 
 
 # */
+import time
+import sys
+import string
 
+#############################################
+# Read file and generate array of number array
+#
+# @pram filename : filename read
+#############################################
+def read_file(filename):
+    numbers = []
+    for l in open(param[1]).readlines():
+        l=l.translate(string.maketrans("", ""), "[] ")
+        l=l.replace(" ", "")
+        data = l[:-1].split(',')
+        x= []
+        for n in data:
+            x += [int(n)]
+        numbers.append(x)
+    return numbers
 
-
-
-
+#############################################
+# convert array to writable format of array
+#
+# @param array : the array converted
+#############################################
+def convert_int_array(array):
+    str_arr = "["
+    for idx, n in enumerate(array):
+        if idx != 0:
+            str_arr += ","
+        str_arr += str(n)
+    str_arr += "]"
+    return str_arr
 
 # https://en.wikipedia.org/wiki/Maximum_subarray_problem
 # Kadane's algorithm
 def max_subarray(array):
+    t_start = time.clock()
     max_ending_here = max_so_far = 0
-    for x in array:
+    max_from = max_to = 0
+    start = 0
+    for idx, x in enumerate(array):
+        if max_ending_here == 0:
+            start = idx
         max_ending_here = max(0, max_ending_here + x)
+        if max_so_far < max_ending_here:
+            max_from = start
+            max_to = idx
         max_so_far = max(max_so_far, max_ending_here)
-    return max_so_far
+    t_end = time.clock()
+    print ("DP finished in time: %8.8f" %(t_end - t_start))
+
+    return array[max_from:max_to+1]
 
 
 
-myList = [-5, 10, -100, 3, -100, 5, -9]
+param = sys.argv
+if len(param) < 3:
+    print "Give input filename and output filename"
+    exit(-1)
 
-greatest_number = max_subarray(myList)
-print(greatest_number)
+numbers = read_file(param[1])
 
+print numbers
+print "\n"
 
+out = open(param[2], "wt")
+
+for idx, arr in enumerate(numbers):
+    sub_array = max_subarray(arr)
+    greatest_number = 0
+    for n in sub_array:
+        greatest_number += n
+    s = "[DP]The max of this array is:" + str(greatest_number)
+    print s
+    print arr
+    print sub_array
+    print "\n"
+    if idx != 0:
+        out.write("\n")
+    out.write(convert_int_array(arr) + "\n")
+    out.write(convert_int_array(sub_array) + "\n")
+    out.write(str(greatest_number) + "\n")
+out.close()
 
 
 
